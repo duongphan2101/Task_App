9953b7ae9fa16795907d107b305a0bda727394a7
duong <duongphan1912@gmail.com>
2025-08-07 11:37:19 +0700

    reconnect

--- APIServerApp/contexts/DesignTimeDbContextFactory.cs
+++ APIServerApp/contexts/DesignTimeDbContextFactory.cs
@@ -7,8 +7,18 @@ namespace APIServerApp.Context
     {
         public AppDbContext CreateDbContext(string[] args)
         {
+            var envPath = Path.Combine(Directory.GetParent(AppContext.BaseDirectory).FullName, ".env");
+            Env.Load(envPath);
+
+            var host = Env.GetString("DB_HOST");
+            var db = Env.GetString("DB_NAME");
+            var user = Env.GetString("DB_USER");
+            var pass = Env.GetString("DB_PASS");
+
+            var connectionString = $"Data Source={host};Initial Catalog={db};User ID={user};Password={pass};TrustServerCertificate=True";
+
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer("Data Source=ADUONG;Initial Catalog=TASK_API_DB;User ID=sa;Password=123;TrustServerCertificate=True");
+            optionsBuilder.UseSqlServer(connectionString);
 
             return new AppDbContext(optionsBuilder.Options);
         }
